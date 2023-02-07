23edceebafbb22ef149245e4e76ba3db7ce38e3e
main <eperry910@yahoo.com>
2023-02-07 15:34:00 -0500

    Added CoinFlipper project

---
+++ CoinFlipper/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace CoinFlipper
+{
+	public class Flipper
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Coin Flipper Starting ...");
+			bool coin = true;
+			var coinValue = new Random();
+			coin = (coinValue.Next(0, 3) > 1) ? true : false;
+			if (coin)
+			{
+				Console.WriteLine("The coin landed on heads");
+			}
+			else
+			{
+				Console.WriteLine("The coin landed on tails");
+			}
+		}
+	}
+}
