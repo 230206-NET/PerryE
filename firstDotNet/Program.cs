8dd8dc29bda3a34b85cd28648ad881613bcdc2fa
main <eperry910@yahoo.com>
2023-02-07 12:45:09 -0500

    Added firstDotNet project

---
+++ firstDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Starting Program");
+string? userInput = Console.ReadLine();
+Console.WriteLine(userInput);
+
+if (5 > 4){
+Console.WriteLine("5 is greater than 4");
+}
+if (Int32.Parse(userInput) > 5){
+Console.WriteLine($"{userInput} is greater than 5");
+}
+Console.WriteLine($"Ending Program");
