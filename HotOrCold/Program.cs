59f6614e645ad11b8034bfe75bd84a7f1485af2a
main <eperry910@yahoo.com>
2023-02-07 16:14:12 -0500

    The Hot Or Cold Guessing game has now been added

---
+++ HotOrCold/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HotCold
+{
+	public class HotOrCold
+	{
+		public static void Main(String[] args){
+		Console.WriteLine("The program has started");
+		var randNum = new Random();
+		int numToGuess = randNum.Next(20);
+		bool guessedCorrect = false;
+		while (!guessedCorrect)
+		{
+			Console.WriteLine("Please enter a number to guess:");
+			int guessedNum = Int32.Parse(Console.ReadLine());
+			if (guessedNum > numToGuess)
+			{
+				Console.WriteLine("You guessed to high. Please try again");
+				continue;
+			}else if (guessedNum < numToGuess)
+			{
+				Console.WriteLine("Your guess was too low. Please try again");
+			}
+			else
+			{
+				guessedCorrect = true;
+			}
+		}
+			Console.WriteLine("Congratulations. You got the correct number: " + numToGuess);
+		}
+	}
+}
