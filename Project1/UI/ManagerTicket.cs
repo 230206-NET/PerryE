5028491379e8cfe6a814c0017b9a192c809465ee
main <eperry910@yahoo.com>
2023-02-17 23:22:54 -0500

    Use while loop over recursion for ManagerTicket

--- Project1/UI/ManagerTicket.cs
+++ Project1/UI/ManagerTicket.cs
@@ -5,35 +5,65 @@ using DataAccess;
 namespace UI;
 class ManagerTickets{
     private int UserId {get; set;}
-    public ManagerTickets(){
-        while(true){
-            int ticketNum;
-            TicketHelper.displayAllPendingTickets();
-            Console.WriteLine("Please Select a ticket number to approve or deny. To exit, please enter 0");
-            bool validTicket = int.TryParse(Console.ReadLine(), out ticketNum);
-            if (validTicket){
-                if (ticketNum == 0) break;
-                while (true){
-                    int choice;
-                    Console.WriteLine("Would you like to approve [1] or deny [2] the reimbursement?");
-                    bool choiceMade = int.TryParse(Console.ReadLine(), out choice);
-                        if (choiceMade){
-                            if (choice == 1){
-                                TicketHelper.approveTicket(ticketNum);
-                                break;
-                            } else if (choice == 2){
-                                TicketHelper.denyTicket(ticketNum);
-                                break;
-                            }
-                            else Console.WriteLine("Invalid Input. Please Try Again");
-                        } else{
-                            Console.WriteLine("Invalid Input. Please Try Again");
-                        }
-                }
+    public ManagerTickets()
+{
+    while(true){
+        int ticketNum = SelectTicket();
+        if (ticketNum == 0) break;
+        ApproveOrDeny(ticketNum);
+    }
+}
 
-            } else{
-                Console.WriteLine("Invalid Input. Please Try Again");
+private int SelectTicket()
+{
+    while(true){
+    int ticketNum;
+    TicketHelper.displayAllPendingTickets();
+    Console.WriteLine("Please Select a ticket number to approve or deny. To exit, please enter 0");
+    bool validTicket = int.TryParse(Console.ReadLine(), out ticketNum);
+    if (!validTicket) 
+    {
+        Console.WriteLine("Invalid Input. Please Try Again");
+        continue;
+    }
+    return ticketNum;
+    }
+}
+
+private void ApproveOrDeny(int ticketNum)
+{
+    while (true)
+    {
+        Console.WriteLine("Would you like to approve [1] or deny [2] the reimbursement?");
+        bool choiceMade = int.TryParse(Console.ReadLine(), out int choice);
+        if (!choiceMade)
+        {
+            Console.WriteLine("Invalid Input. Please Try Again");
+            continue; // repeat loop to prompt user again
+        }
+        if (choice == 1)
+        {
+            try{
+                TicketHelper.approveTicket(ticketNum);
+                break;
+            } catch(Exception e){
+                Console.WriteLine("Invalid input. Please try again");
+                continue;
+            }
+        } 
+        else if (choice == 2)
+        {
+            try{
+                TicketHelper.denyTicket(ticketNum);
+                break;
+            } catch (Exception e){
+                Console.WriteLine("Invalid Input. Please try again");
             }
         }
+        else 
+        {
+            Console.WriteLine("Invalid Input. Please Try Again");
+        }
     }
+}
 }
