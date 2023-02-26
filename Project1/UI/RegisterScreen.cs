c3aa34826d3555a65e0d0a3809c176af577e89ab
main <eperry910@yahoo.com>
2023-02-26 17:54:00 -0500

    Fixed bug with passwords not hashing

--- Project1/UI/RegisterScreen.cs
+++ Project1/UI/RegisterScreen.cs
@@ -14,22 +14,22 @@ public class Register{
     }
     public Register(){
         Console.WriteLine("Please Enter your first name");
-        string firstName = Console.ReadLine();
+        string firstName = Console.ReadLine()!;
         Console.WriteLine("Please Enter your Last Name");
-        string lastName = Console.ReadLine();
+        string lastName = Console.ReadLine()!;
         Console.WriteLine("Please Enter your desired Username");
         string username = Console.ReadLine()!;
         while(!checkForUsername(username)){
             Console.WriteLine("Username taken. Please try another username");
-            username = Console.ReadLine();
+            username = Console.ReadLine()!;
         }
         Console.WriteLine("Please Enter your Password");
         //Eventually, add code here to hash the password before passing it to the database
         string password = Console.ReadLine()!;
         Console.WriteLine("Please Enter your Phone Number (No parentheses, dashes or spaces)");
         //Needs a RegEx to ensure the phone number is valid
-        string phoneNumber = Console.ReadLine();
+        string phoneNumber = Console.ReadLine()!;
         string hashedPassword = PasswordHelper.HashAndSaltPassword(password);
-        DBAccess.CreateNewUser(username, password, firstName + " " + lastName, phoneNumber);
+        DBAccess.CreateNewUser(username, hashedPassword, firstName + " " + lastName, phoneNumber);
     }
 }
